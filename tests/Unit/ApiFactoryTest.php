5f0a6ca3ac6d7d71857ccd1e4fa75028e6d779dd
moelkomy <mo.ah.elkomy@gmail.com>
2021-10-07 02:24:23 +0200

    Writing unit test for api factory

---
+++ tests/Unit/ApiFactoryTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace CodeBugLab\Tmdb\Tests\Unit;
+
+use CodeBugLab\Tmdb\Exceptions\MethodException;
+use CodeBugLab\Tmdb\Factory\ApiFactory;
+use CodeBugLab\Tmdb\Repository\AbstractRepository;
+use CodeBugLab\Tmdb\Tests\TestCase;
+use CodeBugLab\Tmdb\Url\ApiGenerator;
+
+class ApiFactoryTest extends TestCase
+{
+
+    private $movieId = '54768';
+    private $apiKey = '1548795a12542693247b26as6541d65b';
+    private $pageNumber = '1';
+    private $queryString = 'Jack+Reacher';
+    private $appendString = ['videos','posters','casts'];
+
+    public function test_api_factory_return_page_decorator()
+    {
+        $url = new ApiGenerator(AbstractRepository::$apiUrl, $this->apiKey);
+
+        $url->api("movie/" . $this->movieId);
+
+        $apiFactory = (new ApiFactory())->getDecorator("page", [$this->pageNumber] , $url);
+
+        $this->assertEquals(
+            sprintf(
+                "https://api.themoviedb.org/3/movie/%s?api_key=%s&page=%s",
+                $this->movieId,
+                $this->apiKey,
+                $this->pageNumber
+            ),
+            $apiFactory->getUrl()
+        );
+    }
+
+    public function test_api_factory_return_query_decorator()
+    {
+        $url = new ApiGenerator(AbstractRepository::$apiUrl, $this->apiKey);
+
+        $url->api("movie/" . $this->movieId);
+
+        $apiFactory = (new ApiFactory())->getDecorator("query", [$this->queryString] , $url);
+
+        $this->assertEquals(
+            sprintf(
+                "https://api.themoviedb.org/3/movie/%s?api_key=%s&query=%s",
+                $this->movieId,
+                $this->apiKey,
+                urlencode($this->queryString)
+            ),
+            $apiFactory->getUrl()
+        );
+    }
+
+
+    public function test_api_factory_return_append_to_response_decorator()
+    {
+        $url = new ApiGenerator(AbstractRepository::$apiUrl, $this->apiKey);
+
+        $url->api("movie/" . $this->movieId);
+
+        $apiFactory = (new ApiFactory())->getDecorator("appendToResponse", [$this->appendString] , $url);
+
+        $this->assertEquals(
+            sprintf(
+                "https://api.themoviedb.org/3/movie/%s?api_key=%s&append_to_response=%s",
+                $this->movieId,
+                $this->apiKey,
+                implode(",", $this->appendString)
+            ),
+            $apiFactory->getUrl()
+        );
+    }
+
+    public function test_api_factory_throw_exception()
+    {
+        $this->expectException(MethodException::class);
+
+        $url = new ApiGenerator(AbstractRepository::$apiUrl, $this->apiKey);
+
+        $url->api("movie/" . $this->movieId);
+
+        $apiFactory = (new ApiFactory())->getDecorator("wrongMethod", [$this->appendString] , $url);
+    }
+}
